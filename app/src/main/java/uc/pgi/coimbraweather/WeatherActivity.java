b41897fae04e6bbf4a9eebf946311b6dec0b361c
mtaoliveira <migueloliveira1990@gmail.com>
2014-11-13 13:39:33 +0000

    Get longitude and latitude from location manager

--- app/src/main/java/uc/pgi/coimbraweather/WeatherActivity.java
+++ app/src/main/java/uc/pgi/coimbraweather/WeatherActivity.java
@@ -1,6 +1,9 @@
 package uc.pgi.coimbraweather;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -25,35 +28,42 @@ public class WeatherActivity extends Activity {
     @InjectView(R.id.imageView) ImageView imageView;
     @InjectView(R.id.label) TextView label;
 
+    private String TAG = getClass().getName();
+    private String url = "https://aqueous-chamber-4634.herokuapp.com";
+    private LocationManager mLocationManager;
+
+    // Define a listener that responds to location updates
+    LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+            Log.i(TAG, "Latitude: " + location.getLatitude() + ", Longitude:" + location.getLongitude());
+
+            stopCheckingLocation();
+            updateInformation(location.getLatitude(), location.getLongitude());
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
         ButterKnife.inject(this);
-        Ion.getDefault(this).configure().setGson(new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create());
 
-        ResponseFuture<Temperature> json = Ion.with(this)
-                .load("https://aqueous-chamber-4634.herokuapp.com/meteo?lat=40.2&lng=-8.4166667")
-                .as(new TypeToken<Temperature>() {
-                });
+        // Check http://developer.android.com/guide/topics/location/strategies.html
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        try {
-            Log.i("Result", "" + json.get().toString());
-            Ion.with(this)
-                    .load("https://aqueous-chamber-4634.herokuapp.com/static/"+json.get().icon+".png")
-                    .withBitmap()
-                    .placeholder(R.drawable.placeholder)
-                    .error(R.drawable.placeholder)
-                    .intoImageView(imageView);
+        // or: LocationManager.GPS_PROVIDER
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0,
+                0, locationListener);
 
-            label.setText(json.get().summary);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
     }
 
 
@@ -78,4 +88,50 @@ public class WeatherActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void updateInformation(double latitude, double longitude) {
+        Ion.getDefault(this).configure().setGson(new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create());
+
+        ResponseFuture<Temperature> json = Ion.with(this)
+                .load(url + "/meteo?lat=" + latitude + "&lng=" + longitude)
+                .as(new TypeToken<Temperature>() {
+                });
+
+        try {
+            Log.i("Result", "" + json.get().toString());
+            Ion.with(this)
+                    .load(url + "/static/"+json.get().icon+".png")
+                    .withBitmap()
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.placeholder)
+                    .intoImageView(imageView);
+
+            label.setText(json.get().summary);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * The logic of deciding when new fixes are no longer necessary might range from
+     * very simple to very complex depending on your application. A short gap between
+     * when the location is acquired and when the location is used, improves the accuracy
+     * of the estimate. Always beware that listening for a long time consumes a lot of battery
+     * power, so as soon as you have the information you need, you should stop listening for updates
+     *
+     * Check: http://developer.android.com/guide/topics/location/strategies.html#StopListening
+     *  */
+
+    private void stopCheckingLocation() {
+        if (mLocationManager != null) {
+            // Remove the listener you previously added
+            mLocationManager.removeUpdates(locationListener);
+
+            Log.d(TAG, "Location Manager was removed ");
+        }
+    }
+
 }
